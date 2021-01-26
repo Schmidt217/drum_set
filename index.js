9653736135399e961d4895dafeba483ad4ece7ef
Schmidt217 <76504940+Schmidt217@users.noreply.github.com>
2021-01-25 20:04:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const tom1 = new Audio("sounds/tom-1.mp3");
+const tom2 = new Audio("sounds/tom-2.mp3");
+const tom3 = new Audio("sounds/tom-3.mp3");
+const tom4 = new Audio("sounds/tom-4.mp3");
+const snare = new Audio("sounds/snare.mp3");
+const crash = new Audio("sounds/crash.mp3");
+const kickBass = new Audio("sounds/kick-bass.mp3");
+const drums = document.querySelectorAll(".drum");
 // Detecting Button Press
 
-for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
-   document.querySelectorAll(".drum")[i].addEventListener('click', function(){
+for(var i = 0; i < drums.length; i++){
+   drums[i].addEventListener('click', function(){
        playSound(this.innerHTML);
-       buttonAnimation(this.innerHTML);
+        buttonAnimation(this.innerHTML);
    });
 }
 
@@ -19,34 +27,34 @@ function playSound(key){
   
     switch (key) {
         case "w":
-            const tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.currentTime = 0
             tom1.play();
             break;
         case "a":
-            const tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.currentTime = 0
             tom2.play();
             break;
         case "s":
-            const tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.currentTime = 0
             tom3.play();
             break;
         case "d":
-            const tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.currentTime = 0
             tom4.play();
             break;
         case "j":
-            const snare = new Audio("sounds/snare.mp3");
+            snare.currentTime = 0
             snare.play();
             break;
         case "k":
-            const crash = new Audio("sounds/crash.mp3");
+            crash.currentTime = 0
             crash.play();
             break;
         case "l":
-            const kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.currentTime = 0
             kickBass.play();
             break;
-        default: console.log(buttonInnerHTML);
+        default: console.log(this.innerHTML);
             break;
     }
 }
